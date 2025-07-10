c4d98bf2478337096e42d43ff4925c63d40c21cc
ramr0ck-2006 <ramprasadsatpathy999@gmail.com>
2025-07-10 16:36:50 +0530

    first commit

---
+++ # Data Preprocessing.py
@@ -0,0 +1,52 @@
+# Data Preprocessing
+input = "Barack Obama went as a prime minister of USA in the year of 2015 . PM MODI is the prime minister of INDIA."
+print(input)
+#(1)lowercase
+lowercase = input.lower()
+print("LOWERCSE = ", lowercase)
+
+#re
+#pip install re
+import re
+lowercase_re = re.sub('2015', '2025', lowercase)
+print("REGULAR EXP1 = ", lowercase_re)
+lowercase_re = re.sub('[a-m]', '*', lowercase)
+print("REGULAR EXP2 = ", lowercase_re)
+lowercase_re = re.sub('\d', '-', lowercase)
+print("REGULAR EXP3 = ", lowercase_re)
+#(2)Tokenization
+import nltk
+from nltk import word_tokenize, sent_tokenize
+
+word_tokens = word_tokenize(input)
+print("WORD TOKENS = ", word_tokens)
+print(len(word_tokens))
+sent_tokens = sent_tokenize(input)
+print("SENT TOKENS = ", sent_tokens)
+print(len(sent_tokens))
+#(3)stopwords Removal
+from nltk.corpus import stopwords
+#print(stopwords.fileids())
+stopwords = set(stopwords.words('english'))
+#print("\n", stopwords)
+
+tokens_stopwords = []
+for token in word_tokens:
+    if token not in stopwords:
+        tokens_stopwords.append(token)
+print(' '.join(tokens_stopwords))
+#Stemmer
+stemming = []
+from nltk import PorterStemmer
+for word in tokens_stopwords:
+    stemming.append(PorterStemmer().stem(word))
+print(stemming)
+#Lemmatizer
+from nltk import WordNetLemmatizer
+lma = []
+for word in tokens_stopwords:
+    lma.append(WordNetLemmatizer().lemmatize(word))
+print(lma)
+#POS Tags
+from nltk import pos_tag
+print(pos_tag(word_tokens))
